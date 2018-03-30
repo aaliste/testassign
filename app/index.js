97d665c1fa31d3645b430533012227d8aaccf9c4
Emil Samuel <aaliste7@gmail.com>
2018-03-30 20:51:36 +0300

    3.

--- app/index.js
+++ app/index.js
@@ -6,34 +6,4 @@ import './style.scss';
 const { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
 const n = { x, y, ...z };
 
-/*function attachCheckboxHandlers() {
-    var el = document.getElementById('toppings');
 
-    var tops = el.getElementsByTagName('input');
-
-    // assign updateTotal function to onclick property of each checkbox
-    for (var i=0, len=tops.length; i<len; i++) {
-        if ( tops[i].type === 'checkbox' ) {
-            tops[i].onclick = updateTotal;
-        }
-    }
-}
-
-function updateTotal(e) {
-    var form = this.form;
-
-    // get current value in total text box, using parseFloat since it is a string
-    var val = parseFloat( form.elements['total'].value );
-
-    // if check box is checked, add its value to val, otherwise subtract it
-    if ( this.checked ) {
-        val += parseFloat(this.value);
-    } else {
-        val -= parseFloat(this.value);
-    }
-
-    // format val with correct number of decimal places
-    // and use it to update value of total text box
-    // EMIL VAATA SIIA
-    const totalCounter = document.querySelector('.total-counter');
-    totalCounter.innerHTML = `${formatDecimal(val)} is the total`; */
